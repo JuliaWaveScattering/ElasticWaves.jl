bc41d8ad1fa4fbbe555a36a8e5a3d69391719e39
Art Gower <arturgower@gmail.com>
2022-11-30 15:46:24 +0000

    Create ElasticWaves.jl

---
+++ src/ElasticWaves.jl
@@ -0,0 +1,12 @@
+module ElasticWaves
+
+using MultipleScattering
+using SpecialFunctions
+using Statistics
+using LinearAlgebra
+
+greet() = print("Hello World!")
+
+include("types.jl")
+
+end # module
