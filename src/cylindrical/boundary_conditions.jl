c59725f7c42f9cab90ef84cbb2539f344263f1ae
Jess Kent <rkent2@sheffield.ac.uk>
2023-08-24 13:28:57 +0100

    number_of_parameters is now length(boundarybasis.basis)

--- src/cylindrical/boundary_conditions.jl
+++ src/cylindrical/boundary_conditions.jl
@@ -25,8 +25,7 @@ function ElasticWave(sim::BearingSimulation)
     basisfouriermodes = [b.fourier_modes for b in boundarybasis.basis]
     basisfouriermodes = hcat(basisfouriermodes...) |> collect
 
-    number_of_parameters = convert(Int,size(basisfouriermodes,2)/2)
-
+    number_of_parameters = length(boundarybasis.basis)
     T = typeof(ω)
 
     kP = ω / bearing.medium.cp;
@@ -60,28 +59,31 @@ function ElasticWave(sim::BearingSimulation)
         end
     
     else
-        A = boundarycondition_system(ω, bearing, sim.boundarydata1.boundarytype, sim.boundarydata2.boundarytype, m)
-        b = [
-            sim.boundarydata1.fourier_modes[m+basis_order+1,:];
-            sim.boundarydata2.fourier_modes[m+basis_order+1,:]
-        ]
+        for m in -basis_order:basis_order
+            A = boundarycondition_system(ω, bearing, sim.boundarydata1.boundarytype, sim.boundarydata2.boundarytype, m)
+            b = [
+                sim.boundarydata1.fourier_modes[m+basis_order+1,:];
+                sim.boundarydata2.fourier_modes[m+basis_order+1,:]
+            ]
 
-        B = reshape(basisfouriermodes[m+basis_order+1,:], 2, number_of_parameters)
-        B = vcat(B, zeros(ComplexF64, 2, number_of_parameters))
-        M = boundarycondition_system(ω, bearing, TractionBoundary(inner = true), TractionBoundary(outer = true), m)
-        prior = M \ B
+            B = reshape(basisfouriermodes[m+basis_order+1,:], 2, number_of_parameters)
+            B = vcat(B, zeros(ComplexF64, 2, number_of_parameters))
+            M = boundarycondition_system(ω, bearing, TractionBoundary(inner = true), TractionBoundary(outer = true), m)
+            prior = M \ B
 
-        x = (A*prior) \ B
+            x = (A*prior) \ b
 
-        a = prior * x
+            a = prior * x
+            
 
-        relative_error = norm(A*a - b) / norm(b)
-        if relative_error > sim.tol
-            @warn "The relative error for the boundary conditions was $(relative_error) for (ω,basis_order) = $((ω,m))"
+            relative_error = norm(A*a - b) / norm(b)
+            if relative_error > sim.tol
+                @warn "The relative error for the boundary conditions was $(relative_error) for (ω,basis_order) = $((ω,m))"
+            end
+
+            coefficients[m + basis_order + 1] = a
+            mode_errors[m + basis_order + 1] = relative_error
         end
-
-        coefficients[m + basis_order + 1] = a
-        mode_errors[m + basis_order + 1] = relative_error
     end
     coefficients = transpose(hcat(coefficients...)) |> collect
 
