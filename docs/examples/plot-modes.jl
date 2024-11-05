5a09bef9872654eae555cfb7cf1920fba30d6486
Artur Gower <arturgower@gmail.com>
2024-11-05 16:02:50 +0000

    add forward inverse cond plots

--- docs/examples/plot-modes.jl
+++ docs/examples/plot-modes.jl
@@ -9,11 +9,8 @@ medium = Elastic(2; ρ = 7000.0, cp = 5000.0 - 0.0im, cs = 3500.0 - 0.0im)
 
 inner_radius = 1.0
 bearing = RollerBearing(medium = medium, 
-    inner_radius = inner_radius, outer_radius = inner_radius * 1.1, 
-    angular_speed = Ω,  
-    rollers_inside = true,
-    number_of_rollers = Z,
-    roller_radius = inner_radius / (typeof(inner_radius)(1.0) + 2.5*Z / (2pi))
+    inner_radius = inner_radius, outer_radius = inner_radius + 0.3, 
+    number_of_rollers = 0,
 )
 
 krs = LinRange(15,40,24400);
@@ -61,34 +58,66 @@ bd2 = BoundaryData(TractionBoundary(outer=true); coefficients =  1e-4 .* [1.0 1.
     fs = [f[1] for f in result_traction.field];
     result_pressure = FrequencySimulationResult(fs, result_traction.x, [ω]);
     
-    plot(result_pressure, ω;
+    plot(result_pressure, result_pressure.ω[1];
         seriestype=:heatmap
         , field_apply = f -> real(f[1])
         # , leg = false,
     );
     plot!(
-        xlims = (-bearing.inner_radius * 0.4, bearing.inner_radius * 0.4)
-        , ylims = (bearing.inner_radius * 0.8, bearing.outer_radius * 1.1)
-        ,frame = :none, title="", xguide ="",yguide =""
+        # xlims = (-bearing.inner_radius * 0.4, bearing.inner_radius * 0.4), 
+        # ylims = (bearing.inner_radius * 0.8, bearing.outer_radius * 1.1), 
+        frame = :none, title="", xguide ="",yguide =""
     )
 
+    dkr = 8.0;
+    dn = 4
+    
+    dkrs = 2.5:dkr:(2dkr+2.5) |> collect
+    dns = 2.0:dn:(dn+2) |> collect
+    
+    dns2 = reverse(Int[n for kr in dkrs, n in dns])
+    dkrs2 = [kr for kr in dkrs, n in dns]
 
-sims = map(eachindex(ωs)) do i
-    # the option only_stable_modes = false means the method will try to solve for modes which are ill posed 
-    method = ModalMethod(regularisation_parameter = δs[i], 
-        tol = tol, 
+
+results = map(eachindex(dkrs2)) do i
+
+    ω = real(medium.cp) * dkrs2[i];
+    
+    bd1 = BoundaryData(TractionBoundary(inner=true); coefficients =  [0.0 0.0], modes = [dns2[i]])
+    bd2 = BoundaryData(TractionBoundary(outer=true); coefficients =  [1.0 1.0], modes = [dns2[i]])
+
+    method = ModalMethod(
+        tol = tol,
         only_stable_modes = false 
     )
-    BearingSimulation(ωs[i], bearing, bd1, bd2; 
+    sim = BearingSimulation(ω, bearing, bd1, bd2; 
         method = method,
         nondimensionalise = true
     ) 
-end
+    wave = ElasticWave(sim)
 
-waves = [ElasticWave(s) for s in sims];
-
-results = map(eachindex(ωs)) do i
-    result_traction = field(inverse_waves[i], bearing, TractionType(); res = 250);
+    result_traction = field(wave, bearing, TractionType(); res = 250);
     fs = [f[1] for f in result_traction.field];
-    result_pressure = FrequencySimulationResult(fs, result_traction.x, [ωs[i]]);
+    result_pressure = FrequencySimulationResult(fs, result_traction.x, [ω]);
 end
+
+gr(size = (1200,800))
+
+ps = map(results) do r
+    plot(r, r.ω[1];
+        seriestype=:heatmap
+        , field_apply = f -> real(f[1])
+        , leg = false,
+    );
+    plot!(
+        # xlims = (-bearing.inner_radius * 0.4, bearing.inner_radius * 0.4), 
+        # ylims = (bearing.inner_radius * 0.8, bearing.outer_radius * 1.1), 
+        frame = :none, title="", xguide ="",yguide =""
+    )
+    plot!(bearing)
+end;
+
+gr(size = (1200,1200))
+plot(ps..., layout = (2, 3))
+
+# savefig("docs/images/modes-r1-1.0-r2-1p3.pdf")
