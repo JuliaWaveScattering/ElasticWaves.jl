849cb3b65c9b05a20d18d3bdd4182fefde074859
matheusloures42 <matheusloures45@gmail.com>
2025-06-30 15:58:33 +0100

    corrections_to_boundary_data_function

--- src/cylindrical/fields.jl
+++ src/cylindrical/fields.jl
@@ -41,6 +41,19 @@ function field_modes(wave::ElasticWave{2}, r::T, FT::FieldType) where T
     return transpose(pmodes + smodes) |> collect
 end
 
+function field_modes(wave::ElasticWave{2}, r::T, FT::FieldType,  boundarytype::BC) where {BC <: BoundaryCondition, T}
+
+    pmodes = hcat([
+        pressure_field_mode(wave.ω, r, wave.medium, wave.potentials[1].modes[i], FT,  boundarytype) * wave.potentials[1].coefficients[:,i]
+    for i = eachindex(wave.potentials[1].modes)]...)
+
+    smodes = hcat([
+        shear_field_mode(wave.ω, r, wave.medium, wave.potentials[2].modes[i], FT, boundarytype) * wave.potentials[2].coefficients[:,i]
+    for i = eachindex(wave.potentials[2].modes)]...)
+
+    return transpose(pmodes + smodes) |> collect
+end
+
 function field(wave::ElasticWave{2}, bearing::RollerBearing, fieldtype::FieldType; kws...)
 
     inner_circle = Circle(bearing.inner_radius)
@@ -102,6 +115,24 @@ function pressure_field_mode(ω::T, r::Union{T,Complex{T}}, medium::Elastic{2}, 
     return hcat(bessel_modes(besselj), bessel_modes(hankelh1))
 end
 
+function pressure_field_mode(ω::T, r::Union{T,Complex{T}}, medium::Elastic{2}, mode::Int, ::DisplacementType, boundarytype::BC) where {BC <: BoundaryCondition, T}
+    
+    kP = ω / medium.cp;
+    n = mode;
+
+    bessel_modes(J::Function) = [kP/2 * (J(n-1, kP*r) - J(n+1, kP*r)), im * n * J(n, kP*r) / r]
+
+    if boundarytype.inner==true
+
+        return hcat(bessel_modes(besselj), 0.0*bessel_modes(hankelh1))
+    
+    else
+
+        return hcat(0.0*bessel_modes(besselj), bessel_modes(hankelh1))
+
+    end
+end
+
 function shear_field_mode(ω::T, r::Union{T,Complex{T}}, medium::Elastic{2}, mode::Int, ::DisplacementType) where T
 
     n = mode;
@@ -111,6 +142,25 @@ function shear_field_mode(ω::T, r::Union{T,Complex{T}}, medium::Elastic{2}, mod
     bessel_modes(J::Function) = [im * n * J(n, kS*r) / r, -kS/2 * (J(n-1, kS*r) - J(n+1, kS*r))]
 
     return hcat(bessel_modes(besselj), bessel_modes(hankelh1))
+end
+
+function shear_field_mode(ω::T, r::Union{T,Complex{T}}, medium::Elastic{2}, mode::Int, ::DisplacementType, boundarytype::BC) where {BC <: BoundaryCondition, T}
+
+    n = mode;
+    cs = medium.cs
+    kS = ω / cs
+
+    bessel_modes(J::Function) = [im * n * J(n, kS*r) / r, -kS/2 * (J(n-1, kS*r) - J(n+1, kS*r))]
+
+    if boundarytype.inner==true
+
+        return hcat(bessel_modes(besselj), 0.0*bessel_modes(hankelh1))
+    
+    else
+
+        return hcat(0.0*bessel_modes(besselj), bessel_modes(hankelh1))
+
+    end
 end
 
 function pressure_field_mode(ω::T, r::Union{T,Complex{T}}, medium::Elastic{2}, mode::Int, ::TractionType) where T
@@ -129,6 +179,31 @@ function pressure_field_mode(ω::T, r::Union{T,Complex{T}}, medium::Elastic{2}, 
     return hcat(bessel_modes(besselj), bessel_modes(hankelh1))
 end
 
+
+function pressure_field_mode(ω::T, r::Union{T,Complex{T}}, medium::Elastic{2}, mode::Int, ::TractionType, boundarytype::BC) where {BC <: BoundaryCondition, T}
+
+    ρ = medium.ρ
+    n = mode;
+    cp = medium.cp; cs = medium.cs
+    kP = ω / cp; kS = ω / cs
+
+    ρs = ρ * cs^2 / r^2
+    bessel_modes(J::Function) = [
+        -ρs * (2kP * r * J(n-1, kP*r) + (- 2n - 2n^2 + kS^2 * r^2) * J(n, kP*r)),
+        im * n * ρs * (kP*r*J(n-1, kP*r) - 2J(n, kP*r) - kP * r * J(1 + n, kP*r))
+    ]
+
+    if boundarytype.inner==true
+
+        return hcat(bessel_modes(besselj), 0.0*bessel_modes(hankelh1))
+    
+    else
+
+        return hcat(0.0*bessel_modes(besselj), bessel_modes(hankelh1))
+
+    end
+end
+
 function shear_field_mode(ω::T, r::Union{T,Complex{T}}, medium::Elastic{2}, mode::Int, ::TractionType) where T
 
     ρ = medium.ρ
@@ -143,4 +218,28 @@ function shear_field_mode(ω::T, r::Union{T,Complex{T}}, medium::Elastic{2}, mod
     ]
 
     return hcat(bessel_modes(besselj), bessel_modes(hankelh1))
+end
+
+function shear_field_mode(ω::T, r::Union{T,Complex{T}}, medium::Elastic{2}, mode::Int, ::TractionType, boundarytype::BC) where {BC <: BoundaryCondition, T}
+
+    ρ = medium.ρ
+    n = mode;
+    cp = medium.cp; cs = medium.cs
+    kP = ω / cp; kS = ω / cs
+
+    ρs = ρ * cs^2 / r^2
+    bessel_modes(J::Function) = [
+        im * n * ρs * (kS*r*J(n-1, kS*r) - 2J(n, kS*r) - kS*r*J(n+1, kS*r)),
+        ρs * (2kS*r*J(n-1, kS*r) + (-2*n - 2*n^2 + kS^2 * r^2) * J(n, kS*r))
+    ]
+
+    if boundarytype.inner==true
+
+        return hcat(bessel_modes(besselj), 0.0*bessel_modes(hankelh1))
+    
+    else
+
+        return hcat(0.0*bessel_modes(besselj), bessel_modes(hankelh1))
+
+    end
 end
--- test/source_boundarydata_test.jl
+++ test/source_boundarydata_test.jl
@@ -43,3 +43,90 @@ using Test
     @test isapprox(norm(source_matrix4*coes - test_data4), 0.0; atol=1.0e-14)
 
 end
+
+@testset "Source boundary_data test" begin
+
+
+    medium = Elastic(2; ρ = 1.0, cp = 1.5, cs = 1.0)
+    r1 = 1.0
+    r2 = 1.5
+    bearing = RollerBearing(
+        medium = medium,
+        inner_radius = r1, outer_radius = r2
+    )
+
+    # we want wavelengths that go from about 1/10 of the bearing thickness radius to about 10 times the bearing radius
+    krs = 0.1:0.1:10.0
+    kps = krs .* (r1 + 0im)
+    ωs = real.(kps .* medium.cp)
+    kss = ωs ./ medium.cs
+
+    # Source location
+    θo = rand(0.0:0.01:2pi)
+    ro = rand(bearing.inner_radius:0.01:bearing.outer_radius)
+    xo, yo = radial_to_cartesian_coordinates([ro,θo])
+    source_location = [xo,yo]
+    p_map = SourceMap(source_location |> transpose, [1.0])
+    s_map = SourceMap(source_location |> transpose, [0.0])
+
+    # Defining modes to be used
+    basis_order = 5
+    sequecence = reshape(hcat([-(1:basis_order |> collect), 1:basis_order |> collect]...) |> transpose, 2*basis_order)
+    modes = vcat([[0], sequecence]...)
+
+    # Defining basic matrices to be used
+    Mfor = [boundarycondition_system(ω, bearing, TractionBoundary(inner=true), TractionBoundary(outer=true), mode) for mode in modes, ω in ωs]
+    Minv = [boundarycondition_system(ω, bearing, DisplacementBoundary(outer=true), TractionBoundary(outer=true), mode) for mode in modes, ω in ωs]
+
+    Rt = [1.0 0.0 1.0 0.0;
+        1.0 0.0 1.0 0.0;
+        0.0 1.0 0.0 1.0;
+        0.0 1.0 0.0 1.0]
+
+    Ru = [0.0 1.0 0.0 1.0;
+        0.0 1.0 0.0 1.0;
+        0.0 1.0 0.0 1.0;
+        0.0 1.0 0.0 1.0]
+
+    Ts = [(Rt .* Mfor[i]) for i in CartesianIndices(Mfor)]
+    Us = [(Ru .* Minv[i]) for i in CartesianIndices(Minv)]
+
+    ## Checking formulas for the translation matrices
+    p_medium = Acoustic(2; ρ = medium.ρ, c = medium.cp)
+    s_medium = Acoustic(2; ρ = medium.ρ, c = medium.cs)
+
+    n = basis_order
+    maximum(abs.(regular_translation_matrix(p_medium, n, 0, ωs[1], [-xo,-yo])[1,:] .- [besselj.(i, kps[1]*ro) .* exp.(-1im*i*θo) for i in -n:n]))
+
+    maximum(abs.(outgoing_translation_matrix(p_medium, n, 0, ωs[1], [-xo,-yo])[1,:] .- [hankelh1.(i, kps[1]*ro) .* exp.(-1im*i*θo) for i in -n:n]))
+
+
+    U = [hankelh1(n,kps[i]*ro)*exp(-1im*n*θo) for n in -n:n, i in eachindex(ωs)]
+    V = [besselj(n,kps[i]*ro)*exp(-1im*n*θo) for n in -n:n, i in eachindex(ωs)]
+
+    U2=[outgoing_translation_matrix(p_medium,n,0,ωs[i],[-xo,-yo]) for i in eachindex(ωs) ]
+    U2=vcat(U2...) |>transpose
+
+
+    source_coeffs = (-im/4).* [hcat(U[:,i], V[:,i], zeros(length(modes)),  zeros(length(modes))) for i in eachindex(ωs)]
+
+    tractions = [hcat([Ts[i,j]*source_coeffs[j][i,:] for i in eachindex(modes)]...) |> transpose for j in eachindex(ωs)]
+
+    data_inv= [hcat([Us[i,j]*source_coeffs[j][i,:] for i in eachindex(modes)]...) |> transpose for j in eachindex(ωs)]
+       
+    #source_coeffs[1]
+    # Recalculating tractions with new boundary data functions
+    bd1 = [boundary_data(ωs[i], bearing, TractionBoundary(inner=true), p_map, s_map, modes).coefficients for i in eachindex(ωs)]
+    bd2 = [boundary_data(ωs[i], bearing, TractionBoundary(outer=true), p_map, s_map, modes).coefficients for i in eachindex(ωs)]
+    tractions_bd = [hcat(bd1[i],bd2[i]) for i in eachindex(ωs)]
+
+    @test maximum([maximum(abs,tractions_bd[i] .- tractions[i]) for i in eachindex(ωs)]) <1e-7
+
+
+    bd1_inv = [boundary_data(ωs[i], bearing, DisplacementBoundary(outer=true), p_map, s_map, modes).coefficients for i in eachindex(ωs)]
+    bd2_inv = [boundary_data(ωs[i], bearing, TractionBoundary(outer=true), p_map, s_map, modes).coefficients for i in eachindex(ωs)]
+    data_inv_bd = [hcat(bd1_inv[i],bd2_inv[i]) for i in eachindex(ωs)]
+
+    @test maximum([maximum(abs,data_inv[i] .- data_inv_bd[i]) for i in eachindex(ωs)]) <1e-7
+
+end
